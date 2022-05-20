b390506a2ce6196a821067b45c11e3a28d6d5790
Gagan Singh <gagansingh.dev@outlook.com>
2022-05-20 15:13:48 -0400

    Implemented drag and drop functionality in components

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import { addTask } from "../state/actions";
+import React, { useRef } from "react";
+import { useDrop } from "react-dnd";
+import { throttle } from "throttle-debounce-ts";
+import { useItemDrag } from "../hooks/useItemDrag";
+import { addTask, moveList } from "../state/actions";
 import { useAppState } from "../state/AppStateContext";
 import { ColumnContainer, ColumnTitle } from "../styles";
+import { isHidden } from "../utils/isHidden";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 
@@ -21,11 +25,29 @@ type ColumnProps = {
 // }>;
 
 export const Column: React.FC<ColumnProps> = ({ id, text }) => {
-  const { getTasksByListId, dispatch } = useAppState();
+  const { getTasksByListId, dispatch, draggedItem } = useAppState();
   const tasks = getTasksByListId(id);
 
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({ type: "COLUMN", id, text });
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover: throttle(200, () => {
+      if (!draggedItem) {
+        return;
+      }
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) {
+          return;
+        }
+        dispatch(moveList(draggedItem.id, id));
+      }
+    }),
+  });
+
+  drag(drop(ref));
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref} isHidden={isHidden(draggedItem, "COLUMN", id)}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card key={task.id} {...task} />
